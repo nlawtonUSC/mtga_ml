0080006d0539be7493710853a241f3ba8911103f
Neal Lawton <nlawton@usc.edu>
2022-11-05 22:16:54 -0700

    add __version__

--- mtga_ml/__init__.py
+++ mtga_ml/__init__.py
@@ -1,3 +1,7 @@
 """
 mtga_ml - a library of machine learning models and tools for analyzing 17lands MTGA data.
 """
+
+import pkgutil
+
+__version__ = (pkgutil.get_data(__package__, "VERSION") or b"").decode("ascii").strip()
