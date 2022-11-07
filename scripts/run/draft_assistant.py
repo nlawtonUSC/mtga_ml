7e0f6f0e8f43b55c1d04c297cf7063241f80fed4
Neal Lawton <nlawton@usc.edu>
2022-11-06 21:59:12 -0800

    add draft assistant.

---
+++ scripts/run/draft_assistant.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('')
+
+import argparse
+
+import torch
+import numpy as np
+
+class DraftAssistant:
+	def __init__(self, model):
+		self.model = model
+		self.card_names = model.card_names
+		self.lower_card_names = [ x.lower() for x in self.card_names ]
+		self.num_cards = len(self.card_names)
+		self.pack = torch.zeros(self.num_cards)
+		self.pool = torch.zeros(self.num_cards)
+
+	def findCardIdx(self, card_name):
+		"""Find the card whose name contains `substr`."""
+		if card_name == "":
+			return []
+		card_name = card_name.lower()
+		matches = [ i for (i,x) in enumerate(self.lower_card_names) if card_name in x ]
+		if len(matches) == 0:
+			print(f"WARNING: No matches found for \"{card_name}\"")
+			return matches
+		elif len(matches) > 1:
+			print(f"WARNING: Multiple card matches for \"{card_name}\": {', '.join([self.card_names[i] for i in matches])}")
+			return matches
+		return matches
+
+	def predict(self):
+		"""Print model pick predictions"""
+		draft_state = {
+			"pool" : self.pool,
+			"pack" : self.pack
+		}
+		score = self.model.pick(draft_state).detach()
+		perm = np.argsort(-score)
+		k = int(sum(self.pack).cpu().detach().numpy())
+		print("**predictions**:")
+		for i in range(k):
+			print(f"{self.card_names[perm[i]]}: {100 * score[perm[i]]:.2f}%")
+
+	def add_pack(self, card_name):
+		"""Add a card to the pack"""
+		card_idx = self.findCardIdx(card_name)
+		if len(card_idx) == 1:
+			card_idx = card_idx[0]
+			self.pack[card_idx] += 1
+			print(f"**packed: {self.card_names[card_idx]}**")
+
+	def pick(self, card_name):
+		"""Pick a card from the current pack and add it to the pool"""
+		card_idx = self.findCardIdx(card_name)
+		if len(card_idx) == 1:
+			card_idx = card_idx[0]
+			self.pool[card_idx] += 1
+			self.pack = torch.zeros(self.num_cards)
+			print(f"**picked: {self.card_names[card_idx]}**")
+
+	def rm_pack(self, card_name):
+		"""Remove a card from the current pack"""
+		card_idx = self.findCardIdx(card_name)
+		if len(card_idx) == 1:
+			card_idx = card_idx[0]
+			self.pack[card_idx] -= 1
+			print(f"**unpacked: {self.card_names[card_idx]}**")
+
+	def rm_pool(self, card_name):
+		"""Remove a card from the current pool"""
+		card_idx = self.findCardIdx(card_name)
+		if len(card_idx) == 1:
+			card_idx = card_idx[0]
+			self.pool[card_idx] -= 1
+			print(f"**unpooled: {self.card_names[card_idx]}**")
+
+	def pack_list(self):
+		"""Print all cards in the current pack"""
+		print("**packlist**")
+		for idx, count in enumerate(self.pack):
+			if count > 0:
+				print(f"{self.card_names[idx]}: {int(count)}")
+
+	def pool_list(self):
+		"""Print all cards in the current pool"""
+		print("**poollist**")
+		for idx, count in enumerate(self.pool):
+			if count > 0:
+				print(f"{self.card_names[idx]}: {int(count)}")
+
+	def help(self):
+		"""Print help string."""
+		lines = [
+			"Draft assistant running. Enter one of the commands below.",
+			"[CARDS] is a semicolon-separated list of lowercase card name substrings.",
+			"Unrecognized card names are ignored.",
+			"Spaces within card names are not ignored.",
+			"Example:",
+			">>> pack maro;neal;taty",
+			"    **packed: Territorial Maro**",
+			"    WARNING: No matches found for \"neal\"",
+			"    **packed: Tatyova, Steward of Tides**",
+			">>> pack nael",
+			"    **packed: Nael, Avizoa Aeronaut**",
+			">>> packlist",
+			"    **packlist**",
+			"    Nael, Avizoa Aeronaut: 1",
+			"    Tatyova, Steward of Tides: 1",
+			"    Territorial Maro: 1",
+			">>> predict",
+			"    **predictions**:",
+			"    Tatyova, Steward of Tides: 55.46%",
+			"    Territorial Maro: 23.43%",
+			"    Nael, Avizoa Aeronaut: 21.11%",
+			">>> pick steward",
+			"    **picked: Tatyova, Steward of Tides**",
+			">>> poollist",
+			"    **poollist**",
+			"    Tatyova, Steward of Tides: 1",
+			"",
+			"usage: pack [CARDS]",
+			"	Add [CARDS] to the current pack.",
+			"",
+			"usage: pick [CARDS]",
+			"	Add [CARDS] to the pool and clear the current pack.",
+			"",
+			"usage: unpack [CARDS]",
+			"	Remove [CARDS] from the current pack.",
+			"",
+			"usage: unpool [CARDS]",
+			"	Remove [CARDS] from the pool.",
+			"",
+			"usage: predict",
+			"	Call the model to predict the pick from the pack.",
+			"",
+			"usage: packlist",
+			"	Print the names and counts of all cards currently in the pack.",
+			"",
+			"usage: poollist",
+			"	Print the names and counts of all cards currently in the pool.",
+			"",
+			"usage: help",
+			"	Print this message.",
+			"",
+			"usage: exit",
+			"	Exit the program.",
+			"----------"
+		]
+		print('\n'.join(lines))
+
+def main():
+
+	parser = argparse.ArgumentParser(
+		description="Command line UI for using trained a `PickModel` to advise \
+			an MTGA draft."
+	)
+
+	parser.add_argument(
+		"model_path", type=str,
+		help="Path to `PickModel` to load."
+	)
+
+	args = parser.parse_args()
+
+	# load model
+	model_path = args.model_path
+	model = torch.load(model_path)
+
+	# run draft assistant
+	draft_assistant = DraftAssistant(model)
+	draft_assistant.help()
+	while(True):
+		line = input()
+		cmd = line.split(' ')[0]
+		args = ' '.join(line.split(' ')[1:])
+		if cmd == "pack":
+			for card_name in args.split(';'):
+				draft_assistant.add_pack(card_name)
+		elif cmd == "pick":
+			for card_name in args.split(';'):
+				draft_assistant.pick(card_name)
+		elif cmd == "unpack":
+			for card_name in args.split(';'):
+				draft_assistant.rm_pack(card_name)
+		elif cmd == "unpool":
+			for card_name in args.split(';'):
+				draft_assistant.rm_pool(card_name)
+		elif cmd == "predict":
+			draft_assistant.predict()
+		elif cmd == "packlist":
+			draft_assistant.pack_list()
+		elif cmd == "poollist":
+			draft_assistant.pool_list()
+		elif cmd == "help":
+			draft_assistant.help()
+		elif cmd == "exit":
+			exit(0)
+		else:
+			print("command not recognized; try again.")
+
+if __name__ == "__main__":
+	main()
+
+
+
---
+++ scripts/train/pool_pack_pick_predictor.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('')
+
+import argparse
+import os
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import pandas as pd
+from tqdm.auto import tqdm
+
+from mtga_ml.preprocess import load_17lands_data, PicksDataset
+from mtga_ml.ml.model import mlp, PoolPackPickPredictor
+
+def main():
+
+	# Parse arguments
+
+	parser = argparse.ArgumentParser(
+		description="Train an MLP to predict human MTGA draft picks from 17lands data."
+	)
+
+	parser.add_argument(
+		"--data_dir", type=str, default="data/",
+		help="Directory to load 17lands draft data."
+	)
+
+	parser.add_argument(
+		"--mtga_set", type=str, default="DMU", 
+		help="MTGA set identifier, e.g., 'DMU'."
+	)
+
+	parser.add_argument(
+		"--mtga_format", type=str, default="PremierDraft", 
+		help="MTGA format identifier, e.g., 'PremierDraft'."
+	)
+
+	parser.add_argument(
+		"--nrows", type=int, 
+		help="Number of rows to load from the 17lands dataset. If not specified, \
+		uses all data."
+	)
+
+	parser.add_argument(
+		"--checkpoint_dir", type=str, default="checkpoints/",
+		help="Directory to save model checkpoints."
+	)
+
+	parser.add_argument(
+		"--checkpoint_every", type=int, default=1,
+		help="Save a model checkpoint at the end of every `checkpoint_every` epochs. If zero, does not save any checkpoints."
+	)
+
+	parser.add_argument(
+		"--validation_train_split", type=float, default=0.2,
+		help="Fraction of data to be used for model validation."
+	)
+
+	parser.add_argument(
+		"--train_batch_size", type=int, default=8,
+		help="Batch size to use for loading from train dataset."
+	)
+
+	parser.add_argument(
+		"--validation_batch_size", type=int, default=8,
+		help="Batch size to use for loading from validation dataset."
+	)
+
+	parser.add_argument(
+		"--num_epochs", type=int, default=10,
+		help="Number of training epochs."
+	)
+
+	parser.add_argument(
+		"--learning_rate", type=float, default=1e-3,
+		help="Learning rate for training with the Adam optimizer."
+	)
+
+	parser.add_argument(
+		"--h_dims", type=int, nargs='+', default=[50],
+		help="Sequence of ints describing the number of neurons to use in each \
+		layer of the MLP pick prediction model."
+	)
+
+	args = parser.parse_args()
+
+	if not os.path.isdir(args.checkpoint_dir):
+		os.makedirs(args.checkpoint_dir, exist_ok=True)
+
+	# Load 17lands data
+
+	df = load_17lands_data(
+		args.data_dir,
+		args.mtga_set, #"DMU",
+		args.mtga_format, # "PremierDraft",
+		"draft",
+		args.nrows, 
+		force_download=False
+	)
+
+	keys = ["pool", "pack", "pick"]
+	validation_train_split_idx = int(args.validation_train_split * df.shape[0])
+	validation_dataset = PicksDataset(df.loc[:validation_train_split_idx-1], keys)
+	train_dataset = PicksDataset(df.loc[validation_train_split_idx:], keys)
+
+	train_loader = DataLoader(
+		train_dataset, 
+		batch_size=args.train_batch_size,
+		shuffle=True
+	)
+
+	validation_loader = DataLoader(
+		validation_dataset, 
+		batch_size=args.validation_batch_size,
+		shuffle=True
+	)
+
+	card_names = train_dataset.card_names
+	num_cards = train_dataset.num_cards
+
+	# Construct model
+
+	h_dims = [num_cards] + args.h_dims + [num_cards]
+	pred_module = mlp(h_dims, nn.ReLU())
+	model = PoolPackPickPredictor(pred_module, card_names)
+
+	# Training utils
+
+	loss_func = nn.CrossEntropyLoss()
+
+	def accuracy(pred, label):
+		return torch.sum(torch.argmax(pred, -1) == label) / pred.shape[0]
+
+	optimizer = torch.optim.Adam(
+		model.parameters(), 
+		lr=args.learning_rate
+	)
+
+	max_train_steps = args.num_epochs * len(train_dataset) // args.train_batch_size
+
+	max_val_steps = args.num_epochs * len(validation_dataset) // args.validation_batch_size
+
+	progress_bar = tqdm(range(max_train_steps + max_val_steps))
+
+	# Train loop
+	for epoch in range(args.num_epochs):
+
+		# train
+		train_loss = 0
+		train_accuracy = 0
+		train_steps = 0
+		model.train()
+		for batch in train_loader:
+			logits = model(batch)
+			L = loss_func(logits, batch['pick'])
+			optimizer.zero_grad()
+			L.backward()
+			optimizer.step()
+			train_loss += L.item()
+			train_accuracy += accuracy(logits, batch['pick'])
+			train_steps += 1
+			progress_bar.update(1)
+		train_loss /= train_steps
+		train_accuracy /= train_steps
+
+		# eval
+		validation_loss = 0
+		validation_accuracy = 0
+		validation_steps = 0
+		model.eval()
+		for batch in validation_loader:
+			logits = model(batch)
+			L = loss_func(logits, batch['pick'])
+			validation_loss += L.item()
+			validation_accuracy += accuracy(logits, batch['pick'])
+			validation_steps += 1
+			progress_bar.update(1)
+		validation_loss /= validation_steps
+		validation_accuracy /= validation_steps
+
+		# checkpoint
+		if args.checkpoint_every != 0 and epoch % args.checkpoint_every == 0:
+			checkpoint_name = f"checkpoint-{epoch}.pt"
+			checkpoint_path = os.path.join(args.checkpoint_dir, checkpoint_name)
+			torch.save(model, checkpoint_path)
+
+		# print metrics
+		print(f"epoch #{epoch} .. train accuracy {100 * train_accuracy.item():.1f}, validation accuracy {100 * validation_accuracy.item():.1f}")
+
+if __name__ == "__main__":
+	main()
+
+
+
+
